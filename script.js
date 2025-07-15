394fa6babf28b0ebcf37ef778d5eb0a25e175ab4
Jonas <jonas.hanke@gmx.net>
2025-07-15 08:57:15 +0200

    Step5: Funktionierender Warenkorb hinzufügen

--- script.js
+++ script.js
@@ -4,10 +4,35 @@ function renderMenu() {
 
   for (let i = 0; i < menu.length; i++) {
     let meal = menu[i];
-    mealsContainer.innerHTML += getMealTemplate(meal); // ← von template.js
+    mealsContainer.innerHTML += getMealTemplate(meal, i); 
   }
 }
+
 
 window.onload = function () {
   renderMenu();
 };
+
+let cart = [];
+
+function addToCart(index) {
+  let meal = menu[index]; 
+  cart.push(meal); 
+  renderCart(); 
+}
+
+
+function renderCart() {
+  let cartBox = document.querySelector("#basket .cart-content");
+  cartBox.innerHTML = "";
+
+  if (cart.length === 0) {
+    cartBox.innerHTML = "<p>Dein Warenkorb ist leer.</p>";
+    return;
+  }
+
+  for (let i = 0; i < cart.length; i++) {
+    let item = cart[i];
+    cartBox.innerHTML += getCartItemTemplate(item);
+  }
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,12 +1,21 @@
-function getMealTemplate(meal) {
+function getMealTemplate(meal, index) {
   return `
     <div class="meal">
       <div class="meal-title">${meal.name}</div>
       <div class="meal-desc">${meal.description}</div>
       <div class="meal-bottom">
         <div class="meal-price">${meal.price.toFixed(2).replace(".", ",")} €</div>
-        <div class="meal-add">+</div>
+        <div class="meal-add" onclick="addToCart(${index})">+</div>
       </div>
+    </div>
+  `;
+}
+
+function getCartItemTemplate(item) {
+  return `
+    <div class="cart-item">
+      <span>${item.name}</span>
+      <span>${item.price.toFixed(2).replace(".", ",")} €</span>
     </div>
   `;
 }
